810e8f1049978c78368c173d1b8d2f4975e27bb2
Peter Ahlgren <ahlgren1234@gmail.com>
2016-04-10 13:30:36 +0200

    Added All colors.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 $(function() {
     //console.log("Hello World!");
 });
+
+$('.colorbox').click(function() {
+	
+	var code = $(this).attr('data-code');
+	console.log(code);
+	code.execCommand("Copy");
+});
